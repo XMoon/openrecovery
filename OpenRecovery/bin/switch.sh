e6663aa06a3070a6106251a66f39d4d86ac773df
XMoon <a510222107@gmail.com>
2013-01-13 17:26:49 +0800

    修复备份/还原功能 part 3

--- OpenRecovery/bin/switch.sh
+++ OpenRecovery/bin/switch.sh
@@ -66,34 +66,30 @@ cp -f /sdcard/OpenRecovery/etc/keyboard ${ROOT}etc/keyboard
 chmod 0644 ${ROOT}etc/keyboard
 
 # Patch fstab
-if [ $EMMC -eq 0 ]; then
+MTDBLOCK_SYSTEM=$(/sbin/cat /proc/mtd | /sbin/grep "system")
+MTDBLOCK_SYSTEM=${MTDBLOCK_SYSTEM%%:*}
+MTDBLOCK_SYSTEM=${MTDBLOCK_SYSTEM##mtd}
+MTDBLOCK_SYSTEM="\/dev\/block\/mtdblock$MTDBLOCK_SYSTEM"
 
-	MTDBLOCK_SYSTEM=$(/sbin/cat /proc/mtd | /sbin/grep "system")
-	MTDBLOCK_SYSTEM=${MTDBLOCK_SYSTEM%%:*}
-	MTDBLOCK_SYSTEM=${MTDBLOCK_SYSTEM##mtd}
-	MTDBLOCK_SYSTEM="\/dev\/block\/mtdblock$MTDBLOCK_SYSTEM"
-	
-	MTDBLOCK_DATA=$(/sbin/cat /proc/mtd | /sbin/grep "userdata")
-	MTDBLOCK_DATA=${MTDBLOCK_DATA%%:*}
-	MTDBLOCK_DATA=${MTDBLOCK_DATA##mtd}
-	MTDBLOCK_DATA="\/dev\/block\/mtdblock$MTDBLOCK_DATA"
-	
-	MTDBLOCK_CDROM=$(/sbin/cat /proc/mtd | /sbin/grep "cdrom")
-	MTDBLOCK_CDROM=${MTDBLOCK_CDROM%%:*}
-	MTDBLOCK_CDROM=${MTDBLOCK_CDROM##mtd}
-	MTDBLOCK_CDROM="\/dev\/block\/mtdblock$MTDBLOCK_CDROM"
-	
-	MTDBLOCK_CACHE=$(/sbin/cat /proc/mtd | /sbin/grep "cache")
-	MTDBLOCK_CACHE=${MTDBLOCK_CACHE%%:*}
-	MTDBLOCK_CACHE=${MTDBLOCK_CACHE##mtd}
-	MTDBLOCK_CACHE="\/dev\/block\/mtdblock$MTDBLOCK_CACHE"
-	
-	sed -i "s/MTDBLOCKSYSTEM/$MTDBLOCK_SYSTEM/g" /etc/fstab
-	sed -i "s/MTDBLOCKDATA/$MTDBLOCK_DATA/g" /etc/fstab
-	sed -i "s/MTDBLOCKCDROM/$MTDBLOCK_CDROM/g" /etc/fstab
-	sed -i "s/MTDBLOCKCACHE/$MTDBLOCK_CACHE/g" /etc/fstab
-	
-fi
+MTDBLOCK_DATA=$(/sbin/cat /proc/mtd | /sbin/grep "userdata")
+MTDBLOCK_DATA=${MTDBLOCK_DATA%%:*}
+MTDBLOCK_DATA=${MTDBLOCK_DATA##mtd}
+MTDBLOCK_DATA="\/dev\/block\/mtdblock$MTDBLOCK_DATA"
+
+MTDBLOCK_CDROM=$(/sbin/cat /proc/mtd | /sbin/grep "cdrom")
+MTDBLOCK_CDROM=${MTDBLOCK_CDROM%%:*}
+MTDBLOCK_CDROM=${MTDBLOCK_CDROM##mtd}
+MTDBLOCK_CDROM="\/dev\/block\/mtdblock$MTDBLOCK_CDROM"
+
+MTDBLOCK_CACHE=$(/sbin/cat /proc/mtd | /sbin/grep "cache")
+MTDBLOCK_CACHE=${MTDBLOCK_CACHE%%:*}
+MTDBLOCK_CACHE=${MTDBLOCK_CACHE##mtd}
+MTDBLOCK_CACHE="\/dev\/block\/mtdblock$MTDBLOCK_CACHE"
+
+sed -i "s/MTDBLOCKSYSTEM/$MTDBLOCK_SYSTEM/g" /etc/fstab
+sed -i "s/MTDBLOCKDATA/$MTDBLOCK_DATA/g" /etc/fstab
+sed -i "s/MTDBLOCKCDROM/$MTDBLOCK_CDROM/g" /etc/fstab
+sed -i "s/MTDBLOCKCACHE/$MTDBLOCK_CACHE/g" /etc/fstab
 
 #terminfo
 cp -fR /sdcard/OpenRecovery/etc/terminfo/ ${ROOT}etc
