cb7164aa5bfbd61e0b423e7542a67abc53ba054f
XMoon <a510222107@gmail.com>
2013-02-24 19:00:50 +0800

    fix display error

--- OpenRecovery/bin/menu_battery.sh
+++ OpenRecovery/bin/menu_battery.sh
@@ -2,7 +2,7 @@
 
 CAP=`cat /sys/class/power_supply/battery/charge_counter`
 STATUS=`cat /sys/class/power_supply/battery/status`
-echo "Battery - ${CAP}% (${STATU})" > "$MENU_FILE"
+echo "Battery - ${CAP}% (${STATUS})" > "$MENU_FILE"
 echo "Go Back:menu:.." >> "$MENU_FILE"
 echo "*:break:*" >> "$MENU_FILE"
 echo "Wipe Battery Status:shell:wipe_battery.sh" >> "$MENU_FILE"
